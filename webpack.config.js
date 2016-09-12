99e22e0948c0888dda70073df0d6866558cf78e5
erisalke <erisalke@gmail.com>
2016-09-12 19:20:40 +0200

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'html/index.html',
+  inject: 'body'
+});
+
+module.exports = {
+    entry: "./app/app.js",
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          include: __dirname + '/app',
+          loader: 'babel',
+          query: { presets: ["es2015"] }
+        }
+      ]
+    },
+    output: {
+      filename: "js/bundle.js",
+      path: __dirname + '/public'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+  };
