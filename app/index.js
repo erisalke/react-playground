c22e3732445bc13885d460ec2f12ad7c112c093b
erisalke <erisalke@gmail.com>
2016-09-21 14:44:15 +0200

    server side rendering

--- app/index.js
+++ app/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
-import App from './App';
-import About from './modules/About';
-import Repos from './modules/Repos';
-import Repo from './modules/Repo';
-import Home from './modules/Home';
+import { Router, browserHistory } from 'react-router';
+import routes from './modules/routes';
 
 ReactDOM.render(
-  <Router history={ browserHistory }>
-    <Route path="/" component={ App } >
-      <IndexRoute component={Home}/>
-      <Route path="/repos" component={ Repos }>
-        <Route path="/repos/:userName/:repoName" component={ Repo }/>
-      </Route>
-      <Route path="/about" component={ About }/>
-    </Route>
-  </Router>,
+  <Router routes={routes} history={ browserHistory } />,
   document.getElementById('root')
 );
--- app/modules/Repos.js
+++ app/modules/Repos.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { Link } from "react-router";
 import NavLink from "./NavLink.js";
+import { browserHistory } from 'react-router';
 
 export default React.createClass({
-  render: function() {
+  // ask for `router` from context
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  // add this method
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    console.log(path)
+    this.context.router.push(path)
+    // browserHistory.push(path)
+  },
+
+  render() {
     return(
       <div>
         <h2>Repos</h2>
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
